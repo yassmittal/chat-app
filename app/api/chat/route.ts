3f74acca8821343b04cf81bab75ace1e4a87bb97
Yash Mittal <yashmittalmm@gmail.com>
2026-07-02 14:11:39 +0530

    Chore: support amazon bedrock model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,12 +5,17 @@ import {
   createUIMessageStreamResponse,
   toUIMessageStream,
 } from 'ai';
+import { createAmazonBedrock  } from "@ai-sdk/amazon-bedrock";
+
+const bedrockMantle = createAmazonBedrock({
+  region: 'us-east-1'
+});
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
-    model: "xai/grok-build-0.1",
+    model: bedrockMantle("qwen.qwen3-coder-next"),
     messages: await convertToModelMessages(messages),
   });
 
